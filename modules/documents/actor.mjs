ecad40ee8785f03f79b3dddf7f72663269ebe537
KitCat <katsulynx@gmail.com>
2024-03-29 09:02:30 +0000

    Remove carry capacity tooltip Disable carry bonus and multiplier being applied in all cases

--- modules/documents/actor.mjs
+++ modules/documents/actor.mjs
@@ -3,8 +3,6 @@ export function extendActorTemplate(ActorTemplate) {
     return class NoCarryWeightActorTemplate extends ActorTemplate {
 
         _computeEncumbrance() {
-            console.log("Hello World")
-
             const attributes = this.system.attributes;
             if (attributes.encumbrance === undefined) attributes.encumbrance = {};
             const encumbrance = attributes.encumbrance;
@@ -21,10 +19,20 @@ export function extendActorTemplate(ActorTemplate) {
                 acp: 0
             }
         }
+
+        getCarryCapacity() {
+            const carryCapacity = this.system.details.carryCapacity;
+            carryCapacity.bonus.total = 0;
+            carryCapacity.multiplier.total = 1;
+            return super.getCarryCapacity();
+        }
     }
 }
 
 export function removeCarryWeight(sheet, [html], data) {
     html.querySelector(".encumbrance").parentNode.remove();
     html.querySelector(".carry-bonus").remove();
+
+    html.querySelector('.lift').parentNode.classList.remove('tooltip');
+    html.querySelector('.inventory .tab-footer .tooltipcontent').remove();
 }
